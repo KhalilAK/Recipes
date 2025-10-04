cef0d20ae0cfc7d35f63b0213864d139cec9a584
Ameer Khalil <ameerkhalil795@gmail.com>
2025-10-04 06:09:50 -0400

    fix .json

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize("nutrition", "root", "Omarkhalil123", {
-    host: "recipe.render.com",
+const sequelize = new Sequelize("nutrition_wlp9", "nutrition_wlp9_user", "rZekd487WjmqTP2PpLs6OPHbq2DsPHoB", {
+    host: "dpg-d3gf231r0fns73bimta0-a",
     dialect: "postgres",
     port: 5432
 })
